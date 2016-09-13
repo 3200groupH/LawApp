ee11fb655d68001785e7cf3057db5856b4dabd98
3200groupH <21512376@student.uwa.edu.au>
2016-09-13 17:30:32 +0800

    Add files via upload
    Added additional layer of menus accessible through a navigation bar

---
+++ LawApp/LawApp/FutureStudents.swift
@@ -0,0 +1,22 @@
+//
+//  FutureStudents.swift
+//  LawApp
+//
+//  Created by vm on 13/09/2016.
+//  Copyright © 2016 GroupH@uwa.edu.au. All rights reserved.
+//
+
+import UIKit
+
+class FutureStudents: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Navigation
+
+   
+}
---
+++ LawApp/LawApp/Profile.swift
@@ -0,0 +1,22 @@
+//
+//  Profile.swift
+//  LawApp
+//
+//  Created by vm on 13/09/2016.
+//  Copyright © 2016 GroupH@uwa.edu.au. All rights reserved.
+//
+
+import UIKit
+
+class Profile: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Navigation
+
+    
+}

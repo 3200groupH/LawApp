a011e249ead6f25b92917275ed5141b670741bec
3200groupH <21512376@student.uwa.edu.au>
2016-09-29 15:36:15 +0800

    Add files via upload
    Replaced the default navigation behaviour with custom segues. Fully functional but rough visually.

--- LawApp/LawApp/FutureStudents.swift
+++ LawApp/LawApp/FutureStudents.swift
@@ -21,15 +21,12 @@ class FutureStudents: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func ToHome(sender: UIButton) {
+        performSegueWithIdentifier("FutureToHome", sender: self)
     }
-    */
+
+    @IBAction func ToQuiz1(sender: UIButton) {
+        performSegueWithIdentifier("ToFutureQuiz1", sender: self)
+    }
 
 }

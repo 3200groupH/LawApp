ce3c406fb3890b83ed0ee4680291ceee68240da5
3200groupH <21512376@student.uwa.edu.au>
2016-10-15 21:17:47 +0800

    Add files via upload
    Combined the multiple source section of the current student quiz with the future students quiz, and moved the new, longer quiz to the profile section. Added weights to the "Which lawyer are you" quiz, and it now links to a seperate - currently blank - page depending on which type of lawyer scored the highest. Also resolved the warning messages caused by the storyboard elements not being aligned with their constraints.

---
+++ LawApp 5/LawApp/LawApp/AboutPol.swift
@@ -0,0 +1,38 @@
+//
+//  AboutPol.swift
+//  LawApp
+//
+//  Created by vm on 15/10/2016.
+//  Copyright © 2016 GroupH@uwa.edu.au. All rights reserved.
+//
+
+import UIKit
+
+class AboutPol: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func ToHome(_ sender: UIButton) {
+        performSegue(withIdentifier: "PolToHome", sender: self)
+    }
+}
---
+++ LawApp 5/README.md
@@ -0,0 +1,2 @@
+# LawApp
+//this is the readme for our law app
